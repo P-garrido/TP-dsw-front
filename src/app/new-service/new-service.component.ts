428124b81e04b1074387d352a8e675358435b4fa
Pedro <pedrogarrido971@gmail.com>
2023-10-19 20:34:19 -0300

    Retoques en formularios no validos

--- src/app/new-service/new-service.component.ts
+++ src/app/new-service/new-service.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ServicesService } from '../services.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-service',
@@ -8,16 +9,18 @@ import { ServicesService } from '../services.service';
   styleUrls: ['./new-service.component.scss']
 })
 export class NewServiceComponent {
-  constructor(private servicesService: ServicesService) { }
+  constructor(private servicesService: ServicesService, private router: Router) { }
 
 
   newServiceForm = new FormGroup({
     description: new FormControl('', [Validators.required]),
-    price: new FormControl('', [Validators.required])
+    price: new FormControl('', [Validators.required, Validators.min(0)])
   });
 
   addService() {
-    this.servicesService.addService(this.newServiceForm).subscribe();
-    this.newServiceForm.reset();
+    if (this.newServiceForm.valid) {
+      this.servicesService.addService(this.newServiceForm).subscribe();
+      this.router.navigate(['servicios']);
+    }
   }
 }

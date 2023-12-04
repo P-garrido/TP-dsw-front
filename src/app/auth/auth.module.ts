9feb5e595c086dae30d677266075aaa2f0cc1ef2
Pedro <pedrogarrido971@gmail.com>
2023-12-04 18:05:14 -0300

    Arregle problema de link registrarse

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,17 +5,20 @@ import { SignUpComponent } from '../sign-up/sign-up.component';
 import { UsersComponent } from '../users/users.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
 @NgModule({
-  declarations: [LogInComponent,
+  declarations: [
+    LogInComponent,
     SignUpComponent,
     UsersComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     AccordionModule.forRoot(),
+    AppRoutingModule
   ],
   exports: [
     LogInComponent,

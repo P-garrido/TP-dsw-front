e852f2f62ceeae80a164ee630c5f51497f76a178
Pedro <pedrogarrido971@gmail.com>
2023-12-04 11:45:14 -0300

    a

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,29 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
-import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
-import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
-import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
-import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
-import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
-import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
-import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
-import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
-import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
-import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
-import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
-import { ModalModule } from 'ngx-bootstrap/modal';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
-import { AppRoutingModule } from '../app-routing.module';
 import { CartComponent } from './cart.component';
 import { CartProductComponent } from '../cart-product/cart-product.component';
 import { ProductosComponent } from '../productos/productos.component';
@@ -33,6 +12,7 @@ import { OrderItemComponent } from '../order-item/order-item.component';
 import { OffcanvasCartComponent } from '../offcanvas-cart/offcanvas-cart.component';
 import { ServiceComponent } from '../service/service.component';
 import { ProductItemComponent } from '../product-item/product-item.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
@@ -53,6 +33,7 @@ import { ProductItemComponent } from '../product-item/product-item.component';
     ReactiveFormsModule,
     BsDatepickerModule.forRoot(),
     AccordionModule.forRoot(),
+    AppRoutingModule
   ],
   exports: [
     ProductosComponent,

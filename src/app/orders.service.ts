98b5d92e27a1cce217bdf51aabea79b55c4c7bfd
Agustin-Caucino <agucaucino@gmail.com>
2023-12-20 10:51:01 -0300

    agregue handleError a orders

--- src/app/orders.service.ts
+++ src/app/orders.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 import { Order} from './models/order';
 import { Product } from './models/product';
 import { LogInService } from './log-in.service';
@@ -12,10 +12,10 @@ export class OrdersService {
 
   constructor(private http: HttpClient, private logInService: LogInService) { }
   token = this.logInService.token;
-  
+
   loadAllOrders(): Observable<Order[]>{
     const headers = new HttpHeaders().set('Authorization', `Bearer ${this.token}`);
-    return this.http.get<Order[]>('http://localhost:1234/orders', {headers})
+    return this.http.get<Order[]>('http://localhost:1234/orders', {headers}).pipe(catchError(this.handleError))
   }
   getProductById(idProd:number): Observable<Product>{
     const headers = new HttpHeaders().set('Authorization', `Bearer ${this.token}`);
@@ -30,5 +30,15 @@ export class OrdersService {
   getProductByName(prodName:string): Observable<Product>{
     const headers = new HttpHeaders().set('Authorization', `Bearer ${this.token}`);
     return this.http.get<Product>(`http://localhost:1234/products/nombre_producto/${prodName}`, {headers})
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 404) {
+      alert('No se encontraron los pedidos');
+    } else {
+      console.error('Ocurrió un error inesperado:', error.message);
+    }
+
+    return throwError(() => new Error('Ocurrio un error'));
   }
 }
